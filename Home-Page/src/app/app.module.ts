2c636c906e0dab0b7a2eea193a153d3006982457
Andrew Stoerkel <astoer1@students.towson.edu>
2022-05-25 21:34:36 -0400

    new components

--- Home-Page/src/app/app.module.ts
+++ Home-Page/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClockComponent } from './components/clock/clock.component';
+import { ListComponent } from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClockComponent
+    ClockComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
