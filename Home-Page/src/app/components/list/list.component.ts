5ad9eb7f416e3eb2d745dc5fe88bed54e2c9b11e
Andy Stoerkel <astoer1@students.towson.edu>
2022-05-26 12:03:01 -0400

    added shortcut to links

--- Home-Page/src/app/components/list/list.component.ts
+++ Home-Page/src/app/components/list/list.component.ts
@@ -9,9 +9,14 @@ export class ListComponent {
 
   contentList: any[] = [
    {
-     links: ["https://www.github.com", "https://securityinjections.slack.com/ssb/redirect"], content: "Work"
+     links: [
+      "https://www.github.com",
+      "https://securityinjections.slack.com/ssb/redirect", 
+      "https://app.shortcut.com/clarkcan/stories/space/8631/everything"], content: "Work"
    },{
-     links: ["https://www.reddit.com", "https://youtube.com"], content: "Play"
+     links: [
+      "https://www.reddit.com",
+      "https://youtube.com"], content: "Goof"
    }
   ]
   constructor() { }
